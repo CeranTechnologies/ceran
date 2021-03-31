ed3507a931c5a0753d05e8812475a60b58e48e7e
kobtwum <51891724+kobtwum@users.noreply.github.com>
2021-03-31 13:53:30 +0000

    team carousel implemented

--- CeranTemp/assets/js/main.js
+++ CeranTemp/assets/js/main.js
@@ -186,4 +186,28 @@
     aos_init();
   });
 
+      $('#recipeCarousel').carousel({
+    interval: 30000
+  })
+
+  // Team Carousel 
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 5;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+
+
 })(jQuery);
